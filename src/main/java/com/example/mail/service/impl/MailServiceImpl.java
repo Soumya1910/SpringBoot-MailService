6eb93b45e01dcdfc373e7a81d86d57102d5e7b9c
Soumya Mukherjee <rivu10@gmail.com>
2022-05-07 09:08:25 +0530

    Mail send with attachment

--- src/main/java/com/example/mail/service/impl/MailServiceImpl.java
+++ src/main/java/com/example/mail/service/impl/MailServiceImpl.java
@@ -3,12 +3,14 @@ package com.example.mail.service.impl;
 import com.example.mail.pojo.Mail;
 import com.example.mail.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service("mailservice")
 public class MailServiceImpl implements MailService {
@@ -19,12 +21,15 @@ public class MailServiceImpl implements MailService {
     public void sendEmail(Mail mail) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try{
+            FileSystemResource fileSystem
+                    = new FileSystemResource(new File("./sample.txt"));
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
 
             mimeMessageHelper.setSubject(mail.getMailSubject());
             mimeMessageHelper.setFrom(new InternetAddress(mail.getMailFrom(), "Soumya Learning"));
             mimeMessageHelper.setTo(mail.getMailTo());
             mimeMessageHelper.setText(mail.getMailContent());
+            mimeMessageHelper.addAttachment(fileSystem.getFilename(), fileSystem);
 
             javaMailSender.send(mimeMessageHelper.getMimeMessage());
         }
